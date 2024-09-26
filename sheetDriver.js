c6b9c8a8e54b27cd7a739e3c575c58e2f3a0650c
wing <wing@busschots.ie>
2024-09-26 09:56:12 +0100

    Can now handle numbers?

--- sheetDriver.js
+++ sheetDriver.js
@@ -1,18 +1,17 @@
 import MongoConnectionFactory from './MongoConnectionFactory.js';
-import Sheet from './Character Model/Sheet.js';
+import Sheet from './Character Model/GoogleKithainSheet.js';
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const conf = require('./conf.json');
 
+let poolParts = ['dexterity', 'melee', 3];
+
 MongoConnectionFactory.init(conf).then(async function(){
-    Sheet.fromGoogleSheetsURL("https://docs.google.com/spreadsheets/d/1JBbYdH3wVydNUvP804H6UOt4Y5Qi8hSKjh32My3yibY/pubhtml").then(async function(sheet){
-        let sheetJSON = sheet.toJSON();
-        let db = MongoConnectionFactory.getInstance();
-        let entry = await db.collection('sheets').findOneAndUpdate({guildId:"interaction.guildId", userId:"interaction.user.id"}, {$set:{sheet:sheetJSON}}, {upsert:true});
-        await MongoConnectionFactory.close();
-        let recoveredSheetJSON = entry.sheet;
-        let newSheet = await Sheet.fromJSON(recoveredSheetJSON);
-        console.log(newSheet);
-    });
+    Sheet.fromGoogleSheetsURL("https://docs.google.com/spreadsheets/d/1JBbYdH3wVydNUvP804H6UOt4Y5Qi8hSKjh32My3yibY/pubhtml")
+        .then(async function(sheet){
+            let pool = sheet.getPool(poolParts);
+            console.log(pool);
+            MongoConnectionFactory.close();
+        });
 });
